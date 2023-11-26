05f57e69c0729f9d5c7a01c8537eaefa4fae4e1d
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-26 10:46:52 -0300

    Creating domain classes

---
+++ FoodSite/Models/Food.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodSite.Models
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodShortDescription { get; set;}
+        public string FoodDetailedDescription { get; set;}
+        public decimal FoodPrice { get; set;}
+        public string FoodImageUrl { get; set;}
+        public string FoodThumbUrl { get; set;}
+        public bool IsFavoriteFood { get; set;}
+        public bool InStock {  get; set;}
+
+        public int CategoryId { get; set;}
+        public virtual Category Category { get; set;}
+
+    }
+}
