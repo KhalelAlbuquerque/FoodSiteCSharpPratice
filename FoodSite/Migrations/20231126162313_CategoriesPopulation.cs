a8a0679199a86b9b48e0ba599fc9698c5e67882f
Khalel Albuquerque <khaleledipo@gmail.com>
2023-11-26 13:31:28 -0300

    Caterories table population migrate

---
+++ FoodSite/Migrations/20231126162313_CategoriesPopulation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodSite.Migrations
+{
+    public partial class CategoriesPopulation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(CategoryName, CategoryDescription)" +
+                                 "VALUES ('Normal', 'Normal ingredients food')");
+
+
+            migrationBuilder.Sql("INSERT INTO Categories(CategoryName, CategoryDescription)" +
+                                 "VALUES ('Natrual', 'Natural ingredients food')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
